8b66765d65f6d45a2977a3e9fc746cb9446ab51d
義裕脇谷 <yoshihirowakiya@yoshihirowakitani-no-iMac.local>
2013-11-18 21:56:39 +0900

    First comiit

---
+++ .emacs.d/init.el
@@ -0,0 +1,346 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 3.2 Emacsの起動と終了                                  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; P30 デバッグモードでの起動
+;; おまじない
+(require 'cl)
+;; Emacsからの質問をy/nで回答する
+;; (fset 'yes-or-no-p 'y-or-n-p)
+;; スタートアップメッセージを非表示
+(setq inhibit-startup-screen t)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 4.1 効率的な設定ファイルの作り方と管理方法             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P60-61 Elisp配置用のディレクトリを作成
+;; Emacs 23より前のバージョンを利用している方は
+;; user-emacs-directory変数が未定義のため次の設定を追加
+(when (< emacs-major-version 23)
+  (defvar user-emacs-directory "~/.emacs.d/"))
+
+;; load-path を追加する関数を定義
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+              (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
+(add-to-load-path "elisp" "conf" "public_repos")
+
+;; ▼要拡張機能インストール▼（ただし、Emacs24からはインストール不要）
+;;; P115-116 Emacs Lisp Package Archive（ELPA）──Emacs Lispパッケージマネージャ
+;; package.elの設定
+(when (require 'package nil t)
+  ;; パッケージリポジトリにMarmaladeと開発者運営のELPAを追加
+  (add-to-list 'package-archives
+               '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+  ;; インストールしたパッケージにロードパスを通して読み込む
+  (package-initialize))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 4.2 環境に応じた設定の分岐                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P65 CUIとGUIによる分岐
+;; ターミナル以外はツールバー、スクロールバーを非表示
+;;(when window-system
+  ;; tool-barを非表示
+;;  (tool-bar-mode 0)
+  ;; scroll-barを非表示
+;;  (scroll-bar-mode 0))
+
+;; CocoaEmacs以外はメニューバーを非表示
+;;(unless (eq window-system 'ns)
+  ;; menu-barを非表示
+;;  (menu-bar-mode 0))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.2 キーバインドの設定                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P80 C-hをバックスペースにする
+;; 入力されるキーシーケンスを置き換える
+;; ?\C-?はDELのキーシケンス
+;; (keyboard-translate ?\C-h ?\C-?)
+
+;;; P79-81 お勧めのキー操作
+;; C-mにnewline-and-indentを割り当てる。
+;; 先ほどとは異なりglobal-set-keyを利用
+;;(global-set-key (kbd "C-m") 'newline-and-indent)
+;; 折り返しトグルコマンド
+;;(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
+;; "C-t" でウィンドウを切り替える。初期値はtranspose-chars
+(define-key global-map (kbd "C-t") 'other-window)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.3 環境変数の設定                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P82-83 パスの設定
+;;(add-to-list 'exec-path "/opt/local/bin")
+;;(add-to-list 'exec-path "/usr/local/bin")
+;;(add-to-list 'exec-path "~/bin")
+
+;;; P85 文字コードを指定する
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+;;; P86 ファイル名の扱い
+;; Mac OS Xの場合のファイル名の設定
+(when (eq system-type 'darwin)
+  (require 'ucs-normalize)
+  (set-file-name-coding-system 'utf-8-hfs)
+  (setq locale-coding-system 'utf-8-hfs))
+
+;; Windowsの場合のファイル名の設定
+(when (eq window-system 'w32)
+  (set-file-name-coding-system 'cp932)
+  (setq locale-coding-system 'cp932))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.4 フレームに関する設定                               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P87-89 モードラインに関する設定
+;; カラム番号も表示
+(column-number-mode t)
+;; ファイルサイズを表示
+;;(size-indication-mode t)
+;; 時計を表示（好みに応じてフォーマットを変更可能）
+;; (setq display-time-day-and-date t) ; 曜日・月・日を表示
+;; (setq display-time-24hr-format t) ; 24時表示
+;;(display-time-mode t)
+;; バッテリー残量を表示
+;;(display-battery-mode t)
+;; リージョン内の行数と文字数をモードラインに表示する（範囲指定時のみ）
+;; http://d.hatena.ne.jp/sonota88/20110224/1298557375
+(defun count-lines-and-chars ()
+  (if mark-active
+      (format "%d lines,%d chars "
+              (count-lines (region-beginning) (region-end))
+              (- (region-end) (region-beginning)))
+      ;; これだとエコーエリアがチラつく
+      ;;(count-lines-region (region-beginning) (region-end))
+    ""))
+
+(add-to-list 'default-mode-line-format
+             '(:eval (count-lines-and-chars)))
+
+;;; P90 タイトルバーにファイルのフルパスを表示
+(setq frame-title-format "%f")
+;; 行番号を常に表示する
+;; (global-linum-mode t)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.5インデントの設定                                    ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P92-94 タブ文字の表示幅
+;; TABの表示幅。初期値は8
+(setq-default tab-width 4)
+;; インデントにタブ文字を使用しない
+;;(setq-default indent-tabs-mode nil)
+;; php-modeのみタブを利用しない
+;; (add-hook 'php-mode-hook
+;;           '(lambda ()
+;;             (setq indent-tabs-mode nil)))
+
+;; C、C++、JAVA、PHPなどのインデント
+(add-hook 'c-mode-common-hook
+          '(lambda ()
+             (c-set-style "java")))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.6 表示・装飾に関する設定                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P95-96 フェイス
+;; リージョンの背景色を変更
+;; (set-face-background 'region "darkgreen")
+
+;; ▼要拡張機能インストール▼
+;;; P96-97 表示テーマの設定
+;; http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.tar.gz
+(when (require 'color-theme nil t)
+  ;; テーマを読み込むための設定
+  (color-theme-initialize)
+  ;; テーマhoberに変更する
+  (color-theme-billw))
+
+;; ;;; P97-99 フォントの設定
+;; (when (eq window-system 'ns)
+;;   ;; asciiフォントをMenloに
+;;   (set-face-attribute 'default nil
+;;                       :family "Menlo"
+;;                       :height 120)
+;;   ;; 日本語フォントをヒラギノ明朝 Proに
+;;   (set-fontset-font
+;;    nil 'japanese-jisx0208
+;;    ;; 英語名の場合
+;;    ;; (font-spec :family "Hiragino Mincho Pro"))
+;;    (font-spec :family "ヒラギノ明朝 Pro"))
+;;   ;; ひらがなとカタカナをモトヤシーダに
+;;   ;; U+3000-303F	CJKの記号および句読点
+;;   ;; U+3040-309F	ひらがな
+;;   ;; U+30A0-30FF	カタカナ
+;;   (set-fontset-font
+;;    nil '(#x3040 . #x30ff)
+;;    (font-spec :family "NfMotoyaCedar"))
+;;   ;; フォントの横幅を調節する
+;;   (setq face-font-rescale-alist
+;;         '((".*Menlo.*" . 1.0)
+;;           (".*Hiragino_Mincho_Pro.*" . 1.2)
+;;           (".*nfmotoyacedar-bold.*" . 1.2)
+;;           (".*nfmotoyacedar-medium.*" . 1.2)
+;;           ("-cdac$" . 1.3))))
+
+;; (when (eq system-type 'windows-nt)
+;;   ;; asciiフォントをConsolasに
+;;   (set-face-attribute 'default nil
+;;                       :family "Consolas"
+;;                       :height 120)
+;;   ;; 日本語フォントをメイリオに
+;;   (set-fontset-font
+;;    nil
+;;    'japanese-jisx0208
+;;    (font-spec :family "メイリオ"))
+;;   ;; フォントの横幅を調節する
+;;   (setq face-font-rescale-alist
+;;         '((".*Consolas.*" . 1.0)
+;;           (".*メイリオ.*" . 1.15)
+;;           ("-cdac$" . 1.3))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.7 ハイライトの設定                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P100 現在行のハイライト
+(defface my-hl-line-face
+  ;; 背景がdarkならば背景色を紺に
+  '((((class color) (background dark))
+     (:background "NavyBlue" t))
+    ;; 背景がlightならば背景色を緑に
+    (((class color) (background light))
+     (:background "LightGoldenrodYellow" t))
+    (t (:bold t)))
+  "hl-line's my face")
+(setq hl-line-face 'my-hl-line-facep)
+(global-hl-line-mode t)
+
+;; P101 括弧の対応関係のハイライト
+;; paren-mode：対応する括弧を強調して表示する
+(setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
+(show-paren-mode t) ; 有効化
+;; parenのスタイル: expressionは括弧内も強調表示
+(setq show-paren-style 'expression)
+;; フェイスを変更する
+;; (set-face-background 'show-paren-match-face nil)
+;; (set-face-underline-p 'show-paren-match-face "yellow")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.8 バックアップとオートセーブ                         ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P102-103 バックアップとオートセーブの設定
+;; バックアップファイルを作成しない
+;; (setq make-backup-files nil) ; 初期値はt
+;; オートセーブファイルを作らない
+;; (setq auto-save-default nil) ; 初期値はt
+
+;; バックアップファイルの作成場所をシステムのTempディレクトリに変更する
+;; (setq backup-directory-alist
+;;       `((".*" . ,temporary-file-directory)))
+;; オートセーブファイルの作成場所をシステムのTempディレクトリに変更する
+;; (setq auto-save-file-name-transforms
+;;       `((".*" ,temporary-file-directory t)))
+
+;; バックアップとオートセーブファイルを~/.emacs.d/backups/へ集める
+(add-to-list 'backup-directory-alist
+             (cons "." "~/.emacs.d/backups/"))
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "~/.emacs.d/backups/") t)))
+
+;; オートセーブファイル作成までの秒間隔
+;; (setq auto-save-timeout 15)
+;; ;; オートセーブファイル作成までのタイプ間隔
+;; (setq auto-save-interval 60)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.9 フック                                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ファイルが #! から始まる場合、+xを付けて保存する
+(add-hook 'after-save-hook
+          'executable-make-buffer-file-executable-if-script-p)
+
+;; emacs-lisp-mode-hook用の関数を定義
+(defun elisp-mode-hooks ()
+  "lisp-mode-hooks"
+  (when (require 'eldoc nil t)
+    (setq eldoc-idle-delay 0.2)
+    (setq eldoc-echo-area-use-multiline-p t)
+    (turn-on-eldoc-mode)))
+
+;; emacs-lisp-modeのフックをセット
+(add-hook 'emacs-lisp-mode-hook 'elisp-mode-hooks)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.1 Elispをインストールしよう                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ▼要拡張機能インストール▼
+;;; P113 拡張機能を自動インストール──auto-install
+;; auto-installの設定
+(when (require 'auto-install nil t)	; ←1●
+  ;; 2●インストールディレクトリを設定する 初期値は ~/.emacs.d/auto-install/
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  ;; EmacsWikiに登録されているelisp の名前を取得する
+  (auto-install-update-emacswiki-package-name t)
+  ;; 必要であればプロキシの設定を行う
+  ;; (setq url-proxy-services '(("http" . "localhost:8339")))
+  ;; 3●install-elisp の関数を利用可能にする
+  (auto-install-compatibility-setup)) ; 4●
+
+;; ▼要拡張機能インストール▼
+;;; P114-115 auto-installを利用する
+;; (install-elisp "http://www.emacswiki.org/emacs/download/redo+.el")
+(when (require 'redo+ nil t)
+  ;; C-' にリドゥを割り当てる
+  (global-set-key (kbd "C-'") 'redo)
+  ;; 日本語キーボードの場合C-. などがよいかも
+  ;; (global-set-key (kbd "C-.") 'redo)
+  ) ; ←ここでC-x C-eで設定反映
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 7.9 シェルの利用                                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ▼要拡張機能インストール▼
+;;; ターミナルの利用 multi-term
+;; multi-termの設定
+(when (require 'multi-term nil t)
+  ;; 使用するシェルを指定
+  ;; (setq multi-term-program "/bin/bash"))
+  (setq multi-term-program "/bin/zsh"))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 7.10 TRAMPによるサーバ接続                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P225 バックアップファイルを作成しない
+;; TRAMPでバックアップファイルを作成しない
+(add-to-list 'backup-directory-alist
+             (cons tramp-file-name-regexp nil))
