ee9308bde46fa7fff461632b661ef85443fe599b
義裕脇谷 <yoshihirowakiya@yoshihirowakitani-no-iMac.local>
2013-12-11 22:59:00 +0900

    install only
    auto-async-byte-compile.el
    
    lispxmp.el
    
    open-junk-file.el
    
    paredit.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -881,7 +881,7 @@
 (define-key global-map (kbd "s-z") 'my-pop-killed-file-name-list)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;              rubikitch                                 ;;
+;;              rubikitch technique                       ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (tool-bar-mode -1)
@@ -976,6 +976,42 @@
 
 ;; p154 正規表現置換
 (defalias 'qrr 'query-replace-regexp)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;              rubikitch lisp                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; http://www.mag2.com/sample/0001373131
+
+;;;試行錯誤用ファイルを開くための設定
+;; (require 'open-junk-file) 
+;; ;; C-x C-zで試行錯誤ファイルを開く
+;; (global-set-key (kbd "C-x C-z") 'open-junk-file) 
+;; ;;;式の評価結果を注釈するための設定
+;; (require 'lispxmp) 
+;; ;; emacs-lisp-modeでC-c C-dを押すと注釈される
+;; (define-key emacs-lisp-mode-map (kbd "C-c C-d") 'lispxmp) 
+;; ;;;括弧の対応を保持して編集する設定
+;; (require 'paredit) 
+;; (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode) 
+;; (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode) 
+;; (add-hook 'lisp-mode-hook 'enable-paredit-mode) 
+;; (add-hook 'ielm-mode-hook 'enable-paredit-mode) 
+;; (require 'auto-async-byte-compile) 
+;; ;;自動バイトコンパイルを無効にするファイル名の正規表現
+;; (setq auto-async-byte-compile-exclude-files-regexp "/junk/") 
+;; (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode) 
+;; (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode) 
+;; (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode) 
+;; (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode) 
+;; (setq eldoc-idle-delay 0.2) ;すぐに表示したい
+;; (setq eldoc-minor-mode-string "") ;モードラインにElDocと表示しない
+;; ;;釣り合いのとれる括弧をハイライトする
+;; (show-paren-mode 1)
+;; ;;改行と同時にインデントも行う
+;; (global-set-key "\C-m" 'newline-and-indent)
+;; ;; find-functionをキー割り当てする
+;; (find-function-setup-keys)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                     wa                                 ;;
