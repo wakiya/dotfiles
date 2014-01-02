87abe333f98b2f3b0afb8ecdb7e148c6510a4242
義裕脇谷 <yoshihirowakiya@yoshihirowakitani-no-iMac.local>
2014-01-02 16:07:30 +0900

    scroll setting2 recenter 15

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1231,7 +1231,7 @@
   "Scroll current line to top of window.
 Replaces three keystroke sequence C-u 0 C-l."
   (interactive)
-  (recenter 20))
+  (recenter 15))
 
 (global-set-key [f6] 'line-to-top-of-window)
 
