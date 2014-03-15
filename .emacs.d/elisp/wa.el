8cdeedc23a6f576bd30bdf4563101f2fff410c40
wa <y.wakiya@nikkan-pro.co.jp>
2014-03-16 01:49:31 +0900

    wa.el add wa-kill-line-without-kill-ring
    (cherry picked from commit ed7aae550479efe1eac54bacf7cfc6bd9c277edc)

--- .emacs.d/elisp/wa.el
+++ .emacs.d/elisp/wa.el
@@ -1,3 +1,9 @@
+
+;; (when (require 'wa nil t)
+;;   (global-set-key (kbd "M-k") 'wa-copy-this-line)
+;;   (global-set-key (kbd "C-c C-k") 'wa-kill-line-without-kill-ring)
+;; )
+
 (require 'cl)
 (defvar wa-version "0.1")
 
@@ -6,6 +12,13 @@
   (interactive)
   (kill-new (replace-regexp-in-string "\n+$" "" (thing-at-point 'line)))
   (message (car kill-ring)))
+
+(defun wa-kill-line-without-kill-ring ()
+  "Deletes a line, but does not put it in the kill-ring. (kinda)"
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line 1)
+  (setq kill-ring (cdr kill-ring)))
 
 (provide 'wa)
 
