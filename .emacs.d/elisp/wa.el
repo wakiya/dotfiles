61e320656a0ed3ce1c2985ad18e1d67638e40542
wa <y.wakiya@nikkan-pro.co.jp>
2014-03-18 03:24:34 +0900

    wa.el add vs-move-beginning-of-line.el
    wa-kill-line-without-kill-ring change do not kill with new line

--- .emacs.d/elisp/wa.el
+++ .emacs.d/elisp/wa.el
@@ -2,6 +2,7 @@
 ;; (when (require 'wa nil t)
 ;;   (global-set-key (kbd "M-k") 'wa-copy-this-line)
 ;;   (global-set-key (kbd "C-c C-k") 'wa-kill-line-without-kill-ring)
+;;   (global-set-key (kbd "C-a") 'vs-move-beginning-of-line)
 ;; )
 
 (require 'cl)
@@ -17,8 +18,23 @@
   "Deletes a line, but does not put it in the kill-ring. (kinda)"
   (interactive)
   (move-beginning-of-line 1)
-  (kill-line 1)
+  ;; http://flex.phys.tohoku.ac.jp/texi/eljman/eljman_210.html
+  ;; kill-line &optional count Comamnd
+  ;; (kill-line 1) ;;countに１を渡すと改行を含めキルする為、下記に変更
+  (kill-line)
   (setq kill-ring (cdr kill-ring)))
+
+;; https://github.com/ncaq/vs-move-beginning-of-line
+(defun is-reverse-point-whitespace-all ()
+  "カーソルの位置の前には空白文字しかありません"
+  (looking-back "^[\t ]+"))
+
+(defun vs-move-beginning-of-line ()
+  "Visual StdioライクなC-a,通常はインデントに従いHomeへ,もう一度押すと本来のHome"
+  (interactive)
+  (cond
+   ((is-reverse-point-whitespace-all) (move-beginning-of-line nil))
+   (t (back-to-indentation))))
 
 (provide 'wa)
 
