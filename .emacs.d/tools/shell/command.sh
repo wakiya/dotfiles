8f4f6e4122105b345f556027cf1a318812d8f678
wa <y.wakiya@nikkan-pro.co.jp>
2014-07-12 16:26:09 +0900

    tools files change

--- .emacs.d/tools/shell/command.sh
+++ .emacs.d/tools/shell/command.sh
@@ -7,5 +7,10 @@ ls -lt `find save/2014/04/ -name "1000.krn"`
 
 # 指定文字列を含むファイル検索 行番号表示
 find /var/www/TASK/fuel/app/modules/vis/tasks/ -type f -name "*.php" | xargs grep -n "set_krn"
+find /var/www/TASK/fuel/app/modules/vis/ -type f -name "*.php" | xargs grep -n "get_columns"
+find /var/www/TASK/fuel/app/modules/vis/ -type f -name "*.php" | xargs grep -n "mkdir"
+find /var/www/TASK/fuel/app/modules/npr/ -type f -name "*.php" | xargs grep -n "Model_S3::forge();"
 # fuelphp core
 find /var/www/TASK/fuel/core/ -type f -name "*.php" | xargs grep -n "error_info"
+
+find ~/Documents/ -name '*.txt' -ls
